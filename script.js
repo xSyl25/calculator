39a2ccc6feb6d6d922560de5653588ab6775adce
xSYl25 <142290884+xSyl25@users.noreply.github.com>
2023-11-11 03:20:26 -0600

    display values on calculator screen

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+const btns = document.querySelectorAll(".btn");
+const screen = document.querySelector(".screen");
+
+
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        if(!screen.textContent.match(/[0-9]$/) && btn.textContent.match(/[÷x+=]/)) return; 
+        if(screen.textContent.match(/[÷x\-+]$/) && btn.textContent.match(/[÷x+]$/)) return;
+        if(screen.textContent.match(/[\-]$/) && btn.textContent.match(/[\-]/)) return;
+        if(screen.textContent.length === 18) return;
+        display(btn.textContent);
+    })
+});
+
 let num1;
 let num2;
 let operator;
+let displayVal = [];
 
 function add(...numbers) {
     return numbers.reduce((sum, currentVal) => sum + currentVal, 0);
@@ -35,4 +50,11 @@ function operate(num1, operator, num2) {
     if(operator === "-") substract(num1, num2);
     if(operator === "*") multiply(num1, num2);
     if(operator === "/") divide(num1, num2);
+}
+
+function display(value) {
+    if(screen.textContent.length === 9) screen.style.fontSize = "44px";
+    if(screen.textContent.length === 12) screen.style.fontSize = "35px";
+    if(screen.textContent.length === 15) screen.style.fontSize = "29px";
+    displayVal = screen.textContent += value;
 }
